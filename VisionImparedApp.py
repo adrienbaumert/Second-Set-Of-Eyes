7f146b6f4b88e1933805abd2d645c658d401d0c9
adrienbaumert <adrien.baumert@gmail.com>
2023-09-23 16:37:49 -0400

    On hold print hold

--- VisionImparedApp.py
+++ VisionImparedApp.py
@@ -1,11 +1,22 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
+from kivy.clock import Clock
 
 class AppBoxLayout(BoxLayout):
-    pass
+    # Need dt because Clock.schedule_one() automatically passed dt
+    # argument
+    def on_button_hold(self, dt=None):
+        print("Held")
+
+    def on_button_press(self):
+        self.hold_event = Clock.schedule_once(self.on_button_hold, 2)
+
+    def on_button_release(self):
+        self.hold_event.cancel()
 
 class Application(App):
     def build(self):
         Builder.load_file("visionimpared.kv")
         return AppBoxLayout()
+
