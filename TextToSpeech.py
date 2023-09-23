e79e06a008f5516c81f3265302943a01122eb285
adrienbaumert <adrien.baumert@gmail.com>
2023-09-23 12:59:57 -0400

    Updating TextToSpeech class

---
+++ TextToSpeech.py
@@ -0,0 +1,8 @@
+# Importing necessary libraries
+from gtts import gTTS
+
+class Speaker:
+    def tts(self, text):
+        speech = gTTS(text=text, lang="en")
+
+        speech.save("Speech/speechTest.mp4")
