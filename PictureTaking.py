504f74daa7ea741c19b7093a87eb77b45b810771
adrienbaumert <adrien.baumert@gmail.com>
2023-09-23 19:07:40 -0400

    Fixed the image never changing glitch, fixed the camera always been opened glitch

--- PictureTaking.py
+++ PictureTaking.py
@@ -2,16 +2,21 @@
 import cv2
 
 class PictureTaker:
-    def __init__(self):
+    def takePicture(self):
         # Setting camera to default camera
         self.cap = cv2.VideoCapture(0)
 
         # Setting the camera frame rate
         self.cap.set(cv2.CAP_PROP_FPS, 30)
 
-    def takePicture(self):
+        # Setting the camera buffer limit
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
         # Taking a picture
         ret, frame = self.cap.read()
 
         # Saving the picture to a location
         cv2.imwrite("Images/picture.jpg", frame)
+
+        # Releasing the camera
+        self.cap.release()
