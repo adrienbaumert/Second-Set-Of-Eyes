156d2acd8042522c51ca03081491495a5f809bfb
adrienbaumert <adrien.baumert@gmail.com>
2023-09-23 17:40:21 -0400

    Adding something to happen on a double tap of the main button, and something else to happen when the button is held

--- VisionImparedApp.py
+++ VisionImparedApp.py
@@ -2,16 +2,33 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.clock import Clock
+from time import time
 
 class AppBoxLayout(BoxLayout):
+    # Defining a variable for the last time the main button was pressed
+    last_tap_time = 0
+
     # Need dt because Clock.schedule_one() automatically passed dt
     # argument
     def on_button_hold(self, dt=None):
         print("Held")
 
     def on_button_press(self):
-        self.hold_event = Clock.schedule_once(self.on_button_hold, 2)
+        # Schedule an event for button being held
+        self.hold_event = Clock.schedule_once(self.on_button_hold, 1.5)
 
+        # Checking if the last time the main button was pressed was less than .5 seconds
+        # ago
+        current_time = time()
+        if current_time - self.last_tap_time < .5 and self.last_tap_time != 0:
+            self.hold_event.cancel()
+
+            print("Been one second")
+            self.last_tap_time = current_time
+        else:
+            self.last_tap_time = current_time
+
+# Canceling the event scheduled to happen on button hold
     def on_button_release(self):
         self.hold_event.cancel()
 
@@ -19,4 +36,3 @@ class Application(App):
     def build(self):
         Builder.load_file("visionimpared.kv")
         return AppBoxLayout()
-
