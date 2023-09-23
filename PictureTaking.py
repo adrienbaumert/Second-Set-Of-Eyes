7ac0119d27767c3115179cba88b4c7e58ffccf77
adrienbaumert <adrien.baumert@gmail.com>
2023-09-23 12:36:38 -0400

    Update to picture taking class

---
+++ PictureTaking.py
@@ -0,0 +1,16 @@
+# Importing necessary libraries
+import cv2
+class PictureTaker:
+    def __init__(self):
+        # Setting camera to default camera
+        self.cap = cv2.VideoCapture(0)
+
+        # Setting the camera frame rate
+        self.cap.set(cv2.CAP_PROP_FPS, 30)
+
+    def takePicture(self):
+        # Taking a picture
+        ret, frame = self.cap.read()
+
+        # Saving the picture to a location
+        cv2.imwrite("Images/test.jpg", frame)
