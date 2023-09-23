dcd021c3996d7023ec3c079737a3efdecead9b63
Adrien Baumert <63169375+adrienbaumert@users.noreply.github.com>
2023-09-23 14:59:01 -0400

    Update TextToSpeech.py
    Returning the file path of the mp4 and adding a speech function

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,8 +1,14 @@
 # Importing necessary libraries
 from gtts import gTTS
+from playsound import playsound
 
+# Class that goes from text to speech and can play speech
 class Speaker:
     def tts(self, text):
         speech = gTTS(text=text, lang="en")
-
         speech.save("Speech/speech.mp4")
+
+        return "Speech/speech.mp4"
+
+    def speak(self, text):
+        playsound(text)
