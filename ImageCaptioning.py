8c0acb032ba123c3ef17d2a74d77d5c6d0291673
adrienbaumert <adrien.baumert@gmail.com>
2023-09-24 10:36:35 -0400

    File headers

--- ImageCaptioning.py
+++ ImageCaptioning.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2023 Adrien Baumert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Project: Second-Set-Of-Eyes
+# File: ImageCaptioning.py
+# Version : 1.0.0
+# =================================================
 # Importing necessary libraries
 import requests
 from dotenv import load_dotenv
--- PictureTaking.py
+++ PictureTaking.py
@@ -1,10 +1,29 @@
+# Copyright (C) 2023 Adrien Baumert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Project: Second-Set-Of-Eyes
+# File: PictureTaking.py
+# Version : 1.0.0
+# =================================================
 # Importing necessary libraries
 import cv2
 
 class PictureTaker:
     def takePicture(self):
         # Setting camera to default camera
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(4)
 
         # Setting the camera frame rate
         self.cap.set(cv2.CAP_PROP_FPS, 30)
